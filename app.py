df9727adef3d39ff4767723193ea7ff05cec5438
MarkusPagel <markuspagel2@gmail.com>
2025-02-27 12:46:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def get_filter_options():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
